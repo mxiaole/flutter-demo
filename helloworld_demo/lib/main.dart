29384eb9607b6b8fefae668186e11a4512da7b0c
cody <mengjiale6@126.com>
2020-03-11 02:56:11 +0800

    add image widget

--- helloworld_demo/lib/main.dart
+++ helloworld_demo/lib/main.dart
@@ -26,25 +26,38 @@ class MyApp extends StatelessWidget {
           //         decoration: TextDecoration.underline,
           //         decorationStyle: TextDecorationStyle.solid)),
           // 2. Container Widget使用
+          // child: Container(
+          //   child: Text(
+          //     "hello cody",
+          //     style: TextStyle(fontSize: 40),
+          //   ),
+          //   alignment: Alignment.topLeft, // container中的子内容的对齐方式
+          //   width: 500.0,
+          //   height: 400.0,
+          //   // color: Colors.lightBlue,
+          //   padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0), // 设置内边距
+          //   margin: const EdgeInsets.all(20.0), // 设置外边距
+          //   // 设置背景的渐变
+          //   decoration: new BoxDecoration(
+          //       gradient: const LinearGradient(colors: [
+          //         Colors.lightBlue,
+          //         Colors.greenAccent,
+          //         Colors.purple
+          //       ]),
+          //       border: Border.all(width: 2.0, color: Colors.purple)),
+          // ),
+          // 3. image图片 Widget
           child: Container(
-            child: Text(
-              "hello cody",
-              style: TextStyle(fontSize: 40),
+            child: new Image.network(
+              "https://mxiaole.github.io/2020/02/05/jupyter-notebook%E4%BD%BF%E7%94%A8/%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0.png",
+              // fit: BoxFit.fill, // 设置图片在容器中的填充方式
+              // color: Colors.greenAccent,
+              // colorBlendMode: BlendMode.darken,
+              repeat: ImageRepeat.repeatX,
             ),
-            alignment: Alignment.topLeft, // container中的子内容的对齐方式
-            width: 500.0,
-            height: 400.0,
-            // color: Colors.lightBlue,
-            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0), // 设置内边距
-            margin: const EdgeInsets.all(20.0), // 设置外边距
-            // 设置背景的渐变
-            decoration: new BoxDecoration(
-                gradient: const LinearGradient(colors: [
-                  Colors.lightBlue,
-                  Colors.greenAccent,
-                  Colors.purple
-                ]),
-                border: Border.all(width: 2.0, color: Colors.purple)),
+            width: 300.0,
+            height: 200.0,
+            color: Colors.lightBlue,
           ),
         ),
       ),
